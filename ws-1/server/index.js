a61d985dfe7923150e50199404ec83c07fb8f3e9
ccc.444 <contact@coding-crafting-consulting.com>
2024-09-10 14:50:31 +0700

    socket

--- ws-1/server/index.js
+++ ws-1/server/index.js
@@ -3,6 +3,8 @@ const server = new ws.Server ({ port: '3000' })
 
 server.on ('connection', socket => {
   socket.on ('message', message => {
+    const b = Buffer.from (message)
+    console.log (b.toString ())
     console.log (message)
     socket.send (`${ message }`)
   })
